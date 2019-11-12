b7cfe5c226d79739da607ac5c8680a0d77400d65
Luca Trotter <lucatrotter@gmail.com>
2019-11-12 15:08:11 +0100

    Add creator from decimal

--- roman.py
+++ roman.py
@@ -2,10 +2,15 @@ class Roman:
     """Class for Roman numeral object"""
 
     charMap = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    valMap = {n:c for c,n in charMap.items()}
 
-    def __init__(self, romanString):
-        self.roman = romanString
-        self.decimal = self.decimal()
+    def __init__(self, var):
+        if type(var) == str:
+            self.roman = var
+            self.decimal = self.decimal()
+        if type(var) == int:
+            self.decimal = var
+            self.roman = self.roman()
 
     def __repr__(self):
         return self.roman
@@ -33,3 +38,19 @@ class Roman:
         decomposed.append((letter,count,1))
         return decomposed
 
+    def roman(self):
+        romanList = list()
+        current = self.decimal
+        singleChars = sorted(Roman.valMap.keys(), reverse = True)
+        for i in range(len(singleChars)):
+            val = singleChars[i]
+            count = current // val
+            if 0 < count < 4:
+                romanList.append(count * Roman.valMap[val])
+            elif count == 4:
+                if romanList[-1] == Roman.valMap[singleChars[i-1]]:
+                    romanList[-1] = Roman.valMap[val] + Roman.valMap[singleChars[i-2]]
+                else:
+                    romanList.append(Roman.valMap[val] + Roman.valMap[singleChars[i-1]])
+            current -= count * val
+        return "".join(romanList)
