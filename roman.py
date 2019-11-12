ca98248e90802c6ec426ac90b21dd3fb60f96541
Luca Trotter <lucatrotter@gmail.com>
2019-11-12 11:43:58 +0100

    Create Roman class

---
+++ roman.py
@@ -0,0 +1,10 @@
+class Roman:
+    """Class for Roman numeral object"""
+
+    charMap = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+
+    def __init__(self, romanString):
+        self.roman = romanString
+
+    def decimal(self):
+        pass
