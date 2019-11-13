e477f9b3d6c32c39a0e3f7e4daa0647cdae6488c
Luca Trotter <lucatrotter@gmail.com>
2019-11-13 12:39:07 +0100

    Handle exceptions for input errors

--- roman.py
+++ roman.py
@@ -4,18 +4,28 @@ class Roman:
     charMap = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
     valMap = {n:c for c,n in charMap.items()}
 
-    def __init__(self, var):
-        if type(var) == str:
-            self.roman = var
-            self.decimal = self.decimal()
-        if type(var) == int:
-            self.decimal = var
-            self.roman = self.roman()
+    def __init__(self, inputValue):
+        if type(inputValue) == str:
+            self.roman = inputValue
+            self.decimal = self.fromString()
+            if self.fromInt() != self.roman:
+                msg = "Input string is not a legal roman numeral, do you mean \"{}\"?".format(self.fromInt())
+                raise InputError(msg)
+        elif type(inputValue) == int:
+            if inputValue < 4000:
+                self.decimal = inputValue
+                self.roman = self.fromInt()
+            else:
+                msg = "Cannot create a roman numeral higher than 3999"
+                raise InputError(msg)
+        else:
+            msg = "A roman numeral can only be created from an integer or a string"
+            raise InputError(msg)
 
     def __repr__(self):
         return self.roman
 
-    def decimal(self):
+    def fromString(self):
         decomposed = self.decompose()
         val = sum(Roman.charMap[c[0]]*c[1]*c[2] for c in decomposed)
         return val
@@ -38,7 +48,7 @@ class Roman:
         decomposed.append((letter,count,1))
         return decomposed
 
-    def roman(self):
+    def fromInt(self):
         romanList = list()
         current = self.decimal
         singleChars = sorted(Roman.valMap.keys(), reverse = True)
@@ -54,3 +64,9 @@ class Roman:
                     romanList.append(Roman.valMap[val] + Roman.valMap[singleChars[i-1]])
             current -= count * val
         return "".join(romanList)
+
+class InputError(Exception):
+    """Exception raised for errors in the input
+    """
+    def __init__(self, message):
+        self.message = message
